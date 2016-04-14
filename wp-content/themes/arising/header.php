10a70a94af35f6f6ed4e5fa43dcadf78a6274e3d
JessicaStrachan <jessica.strachan@1minus1.com>
2016-04-14 12:37:03 +0100

    Header (#4)
    - Added logo,
    - Added title
    - Added social media (letters as placeholders for now, will be icons in the future)
    - Added new colour variable and applied it to all text globally

--- wp-content/themes/arising/header.php
+++ wp-content/themes/arising/header.php
@@ -5,6 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
   <title><?php echo (is_front_page()) ? get_bloginfo('name') . ' - ' . get_bloginfo('description') : wp_title(''); ?></title>
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
+  <link href='https://fonts.googleapis.com/css?family=Grand+Hotel|Abril+Fatface' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<section class="header">
+  <div class="logo" href="#"></div>
+  <div class="title">
+    <h1>Practicing PHP & CMB2 for Wordpress</h1>
+    <h4>By Jessica Strachan</h4>
+  </div>
+  <div class="social-media">
+    <h1>F</h1>
+    <h1>T</h1>
+  </div>
+</section>
