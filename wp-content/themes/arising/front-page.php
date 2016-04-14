24edbdeb5031214ed6226b7111c6e913d0053769
JessicaStrachan <jessica.strachan@1minus1.com>
2016-04-14 15:53:04 +0100

    On this day (#5)
    * On This Day section
    - php template pulled into front page
    - Rearranged folder structure as sass wasn't being picked up
    - Created On This Day template
    
    * Renamed On This Day sass file

--- wp-content/themes/arising/front-page.php
+++ wp-content/themes/arising/front-page.php
@@ -3,4 +3,5 @@
 * Template Name: Home page
 */
 get_header();
-?>
+
+get_template_part('templates/on-this-day', 'tpl');
---
+++ wp-content/themes/arising/templates/on-this-day.php
@@ -0,0 +1,18 @@
+<section class="on-this-day">
+  <div class="wrapper">
+    <div class="left-content">
+
+    </div>
+    <div class="right-content">
+      <div class="right-content-container">
+      <h1>On this day</h1>
+      <h1 class="date">14th April 1865</h1>
+      <div class="description">
+        <p>
+          On this day in 1865, John Wilkes Booth, an actor and Confederate sympathizer, fatally shoots President Abraham Lincoln at a play at Ford’s Theater in Washington, D.C. The attack came only five days after Confederate General Robert E. Lee surrendered his massive army at Appomattox Court House, Virginia, effectively ending the American Civil War.
+        </p>
+      </div>
+    </div>
+    </div>
+  </div>
+</section>
