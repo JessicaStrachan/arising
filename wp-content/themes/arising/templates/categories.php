a49328614767dd54e4cc1005cbefa2f85fe685ef
Jessica Strachan <jessica.strachan@1minus1.com>
2016-04-15 11:16:38 +0100

    Modified Categories section

--- wp-content/themes/arising/templates/categories.php
+++ wp-content/themes/arising/templates/categories.php
@@ -1,7 +1,11 @@
 <section class="categories-section">
-  <div class="categories-block">
-    <div class="category-image">
-      <h1>History</h1>
-    </div>
+  <div class="categories-block-1">
+    <h1>History</h1>
+  </div>
+  <div class="categories-block-2">
+    <h1>Science</h1>
+  </div>
+  <div class="categories-block-3">
+    <h1>Art</h1>
   </div>
 </section>
