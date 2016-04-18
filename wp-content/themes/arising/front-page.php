52e871900568d83b6f15ebbe71c80afbcf114552
Jessica Strachan <jessica.strachan@1minus1.com>
2016-04-18 10:55:59 +0100

    Footer Build

--- wp-content/themes/arising/front-page.php
+++ wp-content/themes/arising/front-page.php
@@ -7,3 +7,5 @@ get_header();
 get_template_part('templates/on-this-day', 'tpl');
 
 get_template_part('templates/categories', 'tpl');
+
+get_template_part('templates/footer', 'tpl');
---
+++ wp-content/themes/arising/templates/footer.php
@@ -0,0 +1,12 @@
+<section class="page-footer">
+  <div class="footer-wrapper">
+    <a class="logo"></a>
+    <div class="footer-social-media">
+      <a href="facebook">Facebook</a>
+      <a href="twitter">Twitter</a>
+    </div>
+    <footer>
+      <cite>&copy; Jessica Strachan 2016</cite>
+    </footer>
+  </div>
+</section>
