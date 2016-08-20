5114674cda1acefea7fc94b912f0901effaa26b3
JessicaStrachan <jessica.strachan@1minus1.com>
2016-08-20 20:46:55 +0100

    Add Raleway font (#14)

--- wp-content/themes/arising/header.php
+++ wp-content/themes/arising/header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
   <title><?php echo (is_front_page()) ? get_bloginfo('name') . ' - ' . get_bloginfo('description') : wp_title(''); ?></title>
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
-  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab|Abril+Fatface|Grand+Hotel' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab|Raleway' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
