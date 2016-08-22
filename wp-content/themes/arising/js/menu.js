e4cd08ccfa89c3a3f3f253299a60c9f2cdbf89ab
JessicaStrachan <jessica.strachan@1minus1.com>
2016-08-22 21:20:05 +0100

    Navigation closing cross (#20)
    * Jquery to animate burger icon into a closing cross
    
    * Fixed positioning of closing cross

--- wp-content/themes/arising/js/menu.js
+++ wp-content/themes/arising/js/menu.js
@@ -2,5 +2,6 @@ jQuery(document).ready(function($) {
   $('body').click(function() {
     console.log('hello');
     $('.js-nav-menu').addClass('active');
+    $('.js-menu-icon').addClass('active');
   });
 });
