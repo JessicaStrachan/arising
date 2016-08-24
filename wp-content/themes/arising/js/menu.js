14049dc036edcc690282d9a573a13f5e787df6d2
Jessica Strachan <jessica.strachan@1minus1.com>
2016-08-24 23:10:39 +0100

    Nav items hover animation

--- wp-content/themes/arising/js/menu.js
+++ wp-content/themes/arising/js/menu.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function($) {
   $('body').click(function() {
-    console.log('hello');
     $('.js-nav-menu').toggleClass('active');
     $('.js-menu-icon').toggleClass('active');
   });
