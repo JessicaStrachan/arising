82e917edbcda7acc8ef6b92b9747a0ef99988c8b
JessicaStrachan <jessica.strachan@1minus1.com>
2016-08-25 13:37:55 +0100

    Navigation hovers (#22)
    * navigation hovers WIP
    - fixed js files issue
    
    * Nav items hover animation

--- wp-content/themes/arising/header.php
+++ wp-content/themes/arising/header.php
@@ -7,7 +7,6 @@
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
   <link href='https://fonts.googleapis.com/css?family=Roboto+Slab|Raleway' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
-  <script src="<?php echo get_template_directory_uri(); ?>/js/menu.js"></script>
 </head>
 <body <?php body_class(); ?>>
 
@@ -28,12 +27,12 @@
   </div>
 </section>
 
-<nav class="nav-menu js-nav-menu">
-    <ul>
-        <li class="nav-menu__item"><a href="#">Home</a></li>
-        <li class="nav-menu__item"><a href="#">Portfolio</a></li>
-        <li class="nav-menu__item"><a href="#">Blog</a></li>
-        <li class="nav-menu__item"><a href="#">Book Reviews</a></li>
-        <li class="nav-menu__item"><a href="#">Contact</a></li>
-    </ul>
-</nav>
+<div class="nav-menu__container js-nav-menu">
+  <nav class="nav-menu">
+    <a class="nav-menu__item" href="#"><span data-hover="Home">Home</span></a>
+    <a class="nav-menu__item" href="#"><span data-hover="Portfolio">Portfolio</span></a>
+    <a class="nav-menu__item" href="#"><span data-hover="Blog">Blog</span></a>
+    <a class="nav-menu__item" href="#"><span data-hover="Book Reviews">Book Reviews</span></a>
+    <a class="nav-menu__item" href="#"><span data-hover="Contact">Contact</span></a>
+  </nav>
+</div>
