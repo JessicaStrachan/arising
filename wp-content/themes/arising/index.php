c35b326d33acb31bdb01f971466107de65731ae7
Jessica Strachan <jessica.strachan@1minus1.com>
2016-09-20 15:22:11 +0100

    Book Reviews page and single post

--- wp-content/themes/arising/index.php
+++ wp-content/themes/arising/index.php
@@ -1,9 +1,43 @@
 <?php
+
+
+$loop = new WP_Query( array('post_type' => 'book_review') );
+
+// $args = array ('post_type' => 'book_reviews');
+// $loop = new WP_Query( $args );
+
 get_header();
-if(have_posts()): while(have_posts()): the_post();
-  the_title();
-  the_content();
+?>
+
+<section class="book-review__listing">
+
+<?php
+if(have_posts()): while($loop->have_posts()): $loop->the_post();
+
+$post_image = wp_get_attachment_image_src(
+  get_post_thumbnail_id(),
+  'full'
+)[0];
+$featured_image = $post_image ?
+  'background-image: url(' . $post_image . ');' :
+  '';
+?>
+
+  <article class="book-review__listing--item">
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?>
+        <div class="listing--image" style="<?php echo $featured_image; ?>"></div>
+      </a>
+  </article>
+
+
+<?php
+  // the_content();
 endwhile; else:
   echo 'Sorry, no posts matched your criteria';
 endif;
+?>
+
+</section>
+
+<?php
 get_footer();
