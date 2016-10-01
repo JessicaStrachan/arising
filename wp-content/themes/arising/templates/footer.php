f604452d247666cbdcd15e56590f6640d3a77cc7
JessicaStrachan <jessica.strachan@1minus1.com>
2016-10-01 11:00:31 +0100

    Hook up urls to social media links (#33)

--- wp-content/themes/arising/templates/footer.php
+++ wp-content/themes/arising/templates/footer.php
@@ -4,11 +4,10 @@
           <a href="#">hello@jessmadeline.com</a>
       </div>
       <div class="footer-wrapper__social">
-        <a href="#"><i class="fa fa-twitter"></i></a>
-        <a href="#"><i class="fa fa-instagram"></i></a>
-        <a href="#"><i class="fa fa-github"></i></a>
-        <a href="#"><i class="fa fa-pinterest"></i></a>
-        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="https://twitter.com/JessMadeline" target="_blank"><i class="fa fa-twitter"></i></a>
+        <a href="https://www.instagram.com/jessmadelinexo/?hl=en" target="_blank"><i class="fa fa-instagram"></i></a>
+        <a href="https://github.com/JessicaStrachan" target="_blank"><i class="fa fa-github"></i></a>
+        <a href="https://linkedin.com/in/jessica-strachan-8aa853105" target="_blank"><i class="fa fa-linkedin"></i></a>
       </div>
       <div class="footer-wrapper__copyright">
         <p>&copy; Jessica Strachan 2016</p>
